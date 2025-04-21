300a688e60f5073515a42ada9fa4e81f1e577cfb
Nipun <nipun@ezekia.com>
2025-04-21 12:47:33 +0100

    Slice array in GO

--- main.go
+++ main.go
@@ -6,11 +6,12 @@ func main() {
 	conferenceName := "Go Conference"
 	const conferenceTickets int = 50
 	var remainingTickets uint = 50
+	var bookings []string // slice to hold bookings - a dynamic array
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
-
+	
 	var firstName string
 	var lastName string
 	var email string
@@ -36,7 +37,15 @@ func main() {
 		fmt.Printf("You need to book at least 1 ticket\n")
 		return
 	}
+
+	bookings = append(bookings, firstName+" "+lastName)
+
+	// fmt.Printf("The whole bookings slice: %v\n", bookings)
+	// fmt.Printf("The first booking is %v\n", bookings[0])
+	// fmt.Printf("Slice type: %T\n", bookings)
+	// fmt.Printf("Slice length: %v\n", len(bookings))
 	
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+	fmt.Printf("These are all our bookings: %v\n", bookings)
 }
